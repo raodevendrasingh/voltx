9fba8ea7e83f70b33d42440d71ab707811138328
Devendra Singh <devthedeveloperguy@gmail.com>
2025-03-20 23:45:26 +0530

    feat(core): Add version and catch-all commands

--- bin/index.tsx
+++ bin/index.tsx
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 import { program } from "commander";
 import { spawn } from "child_process";
+import chalk from "chalk";
 
 program
+	.version("0.1.0", "-v, --version", "Display CLI version")
 	.command("init")
 	.description("Initialize configuration")
 	.action(() => {
@@ -15,5 +17,11 @@ program
 	.action(() => {
 		spawn("npx", ["tsx", "commands/whoami.tsx"], { stdio: "inherit" });
 	});
+
+program.on("command:*", (operands) => {
+	console.error(chalk.red(`Unknown command: ${operands[0]}`));
+	console.log("Run with --help to see available commands.");
+	process.exitCode = 1;
+});
 
 program.parse(process.argv);
