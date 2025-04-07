d7969ec8a2d8fefca03eca85d33d933be78f5db5
Devendra Singh <devthedeveloperguy@gmail.com>
2025-04-07 20:49:46 +0530

    feat(web): add logo and favicon

--- apps/web/app/layout.config.tsx
+++ apps/web/app/layout.config.tsx
@@ -1,4 +1,7 @@
 import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
+import Image from "next/image";
+import wordmarkDark from "@/assets/brand/wordmark_dark.png";
+import wordmarkLight from "@/assets/brand/wordmark_light.png";
 
 /**
  * Shared layout configurations
@@ -11,21 +14,24 @@ export const baseOptions: BaseLayoutProps = {
 	nav: {
 		title: (
 			<>
-				<svg
-					width="24"
-					height="24"
-					xmlns="http://www.w3.org/2000/svg"
-					aria-label="Logo"
-				>
-					<circle cx={12} cy={12} r={12} fill="currentColor" />
-				</svg>
-				VoltX
+				<Image
+					src={wordmarkDark}
+					alt="site_logo"
+					className="dark:hidden block"
+					width={90}
+				/>
+				<Image
+					src={wordmarkLight}
+					alt="site_logo"
+					className="hidden dark:block"
+					width={90}
+				/>
 			</>
 		),
 	},
 	links: [
 		{
-			text: "Documentation",
+			text: "Docs",
 			url: "/docs",
 			active: "nested-url",
 		},
